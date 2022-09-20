1ec665ad75a3bad74a74194267356901b1f957d8
Omar Abouelleil <oabouelleil@live.com>
2022-09-20 19:09:59 +0100

    Add LCIncludes.h

---
+++ LCIncludes.h
@@ -0,0 +1,13 @@
+#ifndef LCUTILS_LCINCLUDES
+#define LCUTILS_LCINCLUDES
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#endif //LCUTILS_LCINCLUDES
