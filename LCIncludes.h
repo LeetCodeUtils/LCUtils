8840731263a1868f4dcf316904e06b22527d36c9
Omar Abouelleil <oabouelleil@live.com>
2022-10-02 21:29:08 +0400

    Update LCIncludes.h

--- LCIncludes.h
+++ LCIncludes.h
@@ -1,12 +1,7 @@
 #ifndef LCUTILS_LCINCLUDES
 #define LCUTILS_LCINCLUDES
 
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
