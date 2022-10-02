8ac7c1dbfe511402bf253ce5b542c9a256144a8c
Omar Abouelleil <oabouelleil@live.com>
2022-10-02 21:28:58 +0400

    Update LCPrint.h

--- LCPrint.h
+++ LCPrint.h
@@ -3,32 +3,39 @@
 
 #include "LCIncludes.h"
 
-template<typename T>
-void LCPrint(const vector<vector<T>> &v) {
-    for (auto i: v) {
-        print(i);
-        cout << endl;
+namespace LC {
+
+    void print() {
+        cout << '\n';
+    }
+
+    template<typename T, typename ...TAIL>
+    void print(const T &t, TAIL... tail) {
+        print(t);
+        print(tail...);
+    }
+
+
+    template<typename T>
+    void print(const vector<vector<T>> &v) {
+        for (size_t i = 0; i < v.size(); ++i) {
+            for (size_t j = 0; j < v[i].size(); ++j) {
+                cout << v[i][j] << ",\t";
+            }
+            cout << endl;
+        }
+    }
+
+    template<typename T>
+    void print(const vector<T> &v) {
+        for (size_t j = 0; j < v.size(); ++j) {
+            cout << v[j] << ",\t";
+        }
+    }
+
+    template<typename T1, typename T2>
+    void print(const pair<T1, T2> &p) {
+        cout << "First: " << p.first << ", Second: " << p.second << endl;
     }
 }
-
-template<typename T>
-void LCPrint(const vector<T> &v) {
-    cout << "[";
-    for (size_t j = 0; j < v.size(); ++j) {
-        cout << v[j] << " ";
-    }
-    cout << "]";
-}
-
-
-void LCPrint() {
-    cout << '\n';
-}
-
-template<typename T, typename ...TAIL>
-void LCPrint(const T &t, TAIL... tail) {
-    cout << t << ' ';
-    print(tail...);
-}
-
-#endif
+#endif //LCPRINT
